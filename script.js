a6f645da6107fb24a61b306660b95c4006d40c9c
VeeraMahendra7 <140475884+VeeraMahendra7@users.noreply.github.com>
2025-10-07 16:50:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function toCase(text) {
   // write your code here
+  let lcase = text.toLowerCase();
+  let ucase = text.toUpperCase();
+  return `${lcase}-${ucase}`;
 }
 
 // DO not change the code below
